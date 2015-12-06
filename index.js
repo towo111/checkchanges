6b1ed663ae5333fca1e0fa7135f1b8f901e4d28d
Tobias <to.wo@web.de>
2015-12-06 20:46:17 +0100

    Added printing timestamp for better notice when the command was executed.

--- index.js
+++ index.js
@@ -16,8 +16,32 @@ for (var i = 2; i < process.argv.length - 1; i++) {
 
 	fs.watch(process.argv[i], { persistent: true, recursive: true }, function(event, filename) {
 		exec(command, function(error, stdout, stderr) {
-			console.log("Command '" + command + "' executed.");
+			console.log("Command '" + command + "' executed (" + getDateTime() + ")");
 		});
 	});
+}
+
+function getDateTime() {
+
+    var date = new Date();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
 }
 
